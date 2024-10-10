aca86c21b2bf678eac983d52c235ba8a4b7d9873
btworeo <andychris1808@gmail.com>
2024-10-10 13:04:03 +0530

    files added

--- clone.py
+++ clone.py
@@ -1,9 +1,4 @@
-# Ported From DarkCobra , Originally By Uniborg
-# Ultroid - UserBot
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# oreo
 
 """
 ✘ Commands Available
